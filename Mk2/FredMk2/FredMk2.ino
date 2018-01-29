49bafce963a39dd38e814a053c3a27867ce6dec5
Jared <jared.rattray@yahoo.com>
2018-01-29 02:19:50 -0600

    Scanning update
    Setup a priority array and changed constants to definitions. Also setup a loop to check the priority array and jump back to scan if no network is connected to.

--- Mk2/FredMk2/FredMk2.ino
+++ Mk2/FredMk2/FredMk2.ino
@@ -1,29 +1,31 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
 
-const char* ssid_controller = "Controller";
-const char* ssid_home = "Jared's Network";
-cosnt char* ssid_main;
+#define ssid_controller "Controller"
+#define ssid_home "Jared's Network"
+#define port 150
 
 WiFiUDP UDP;
-IPAddress control(192, 168, 4, 1);
-IPAddress local(192, 168, 0, 1);
-IPAddress gateway(192, 168, 1, 9);
-IPAddress subnet(255, 255, 255, 0);
-const unsigned int port = 150;
+IPAddress 
+control(192, 168, 4, 1),
+local(192, 168, 0, 1),
+gateway(192, 168, 1, 9),
+subnet(255, 255, 255, 0);
 
 void setup() {
+  char* priority[2];
   int networks_number = WiFi.scanNetworks();
-
-  for(int i = 0; i < networks_number; i++){
-    if(WiFi.SSID(i) == ssid_controller){
-      ssid_main = ssid_controller;
-      i = networks_number;
+  
+  scan:
+  for(int i = 0; i < networks_number; i++){   //Loop to go through the current wireless networks
+    if(WiFi.SSID(i) == ssid_controller){      //looking for specific networks. Puts them in 
+      i = networks_number;                    //specific places in the priority array that 
+      priority[0] = ssid_controller;          //informs the next step.
       return;
     }
     else if(WiFi.SSID(i) == ssid_home){
-      ssid_main = ssid_home;
       i = networks_number;
+      priority[1] = ssid_home;
       return;
     }
     else{
@@ -31,12 +33,24 @@ void setup() {
     }
   }
 
-  Serial.begin(115200);
-  WiFi.config(local, gateway, subnet);
-  WiFi.begin(ssid_main);
-  UDP.begin(port);
-
-  
+  if(priority[0] == ssid_controller){         //Checks the priority array connecting to the
+    Serial.begin(115200);                     //first available in the priority array.
+    WiFi.config(local, gateway, subnet);
+    WiFi.begin(ssid_controller);
+    UDP.begin(port);
+    return;
+  }
+  else if(priority[1] == ssid_home){
+    Serial.begin(115200);
+    WiFi.config(local, gateway, subnet);
+    WiFi.begin(ssid_home);
+    UDP.begin(port);
+    return;
+  }
+  else{
+    goto scan;                                //Jumps back to rescan the networks, repeats until
+    return;                                   //turned off or connected to a specified network.
+  }
 }
 
 void loop() {
