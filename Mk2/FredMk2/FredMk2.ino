17a3517eeddc9674ac2a6bf1d9a84af1e28c5f50
Jared <jared.rattray@yahoo.com>
2018-01-30 00:43:25 -0600

    Setup Packet Reader
    Changed the network connection code, just searches for controller now. New button scheme is just to send a long int that has the button's inputs bitwised together into a specific order.

--- Mk2/FredMk2/FredMk2.ino
+++ Mk2/FredMk2/FredMk2.ino
@@ -1,59 +1,65 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUDP.h>
+#include <stdlib.h>
 
 #define ssid_controller "Controller"
-#define ssid_home "Jared's Network"
 #define port 150
 
 WiFiUDP UDP;
-IPAddress 
-control(192, 168, 4, 1),
-local(192, 168, 0, 1),
-gateway(192, 168, 1, 9),
-subnet(255, 255, 255, 0);
 
 void setup() {
-  char* priority[2];
-  int networks_number = WiFi.scanNetworks();
+ pinMode(2, OUTPUT);
   
-  scan:
-  for(int i = 0; i < networks_number; i++){   //Loop to go through the current wireless networks
-    if(WiFi.SSID(i) == ssid_controller){      //looking for specific networks. Puts them in 
-      i = networks_number;                    //specific places in the priority array that 
-      priority[0] = ssid_controller;          //informs the next step.
-      return;
-    }
-    else if(WiFi.SSID(i) == ssid_home){
-      i = networks_number;
-      priority[1] = ssid_home;
-      return;
-    }
-    else{
-      return;
-    }
-  }
+ Serial.begin(115200); 
+ WiFi.begin(ssid_controller);           //Starts searching for the controller
 
-  if(priority[0] == ssid_controller){         //Checks the priority array connecting to the
-    Serial.begin(115200);                     //first available in the priority array.
-    WiFi.config(local, gateway, subnet);
-    WiFi.begin(ssid_controller);
-    UDP.begin(port);
-    return;
-  }
-  else if(priority[1] == ssid_home){
-    Serial.begin(115200);
-    WiFi.config(local, gateway, subnet);
-    WiFi.begin(ssid_home);
-    UDP.begin(port);
-    return;
-  }
-  else{
-    goto scan;                                //Jumps back to rescan the networks, repeats until
-    return;                                   //turned off or connected to a specified network.
-  }
-}
+ while(WiFi.status() != WL_CONNECTED){  //Waits for a connection blinking the blue light
+   delay(500);
+   digitalWrite(2,HIGH);
+   delay(500);
+   digitalWrite(2,LOW);
+ }
+ UDP.begin(port);                       //Connecting to the port
+ }
+
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  valueset(16777215);
+  delay(2000);
+
+
 
 }
+
+
+
+unsigned long int readpack(){           //Reads packets and converts them to long int
+  char incPack[255];
+  if(UDP.parsePacket()){
+    int packLength = UDP.read(incPack,255);
+    if(packLength > 0){
+      incPack[packLength] = 0;
+    }
+    UDP.flush();
+    char test[255] = {1,2,3,4,5};
+    unsigned long int temp = atoi(test);   
+    return temp;
+  }
+  else{ 
+    return 0;
+  }
+}
+
+void valueset(unsigned long int temp){    //Bitwise operations to seperate out the button clicks
+  unsigned long int x = temp & 0x0003ff;
+  unsigned long int y = (temp >> 10) & 0x0003ff;
+  unsigned long int left = (temp >> 20) & 0x000001;
+  unsigned long int right = (temp >> 21) & 0x000001;
+  unsigned long int up = (temp >> 22) & 0x000001;
+  unsigned long int down = (temp >> 23) & 0x000001;
+
+
+}
+
+
